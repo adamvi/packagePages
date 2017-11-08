3f9694c196081fd1be69abc145b6a131fde70f95
Adam VanIwaarden <avaniwaarden@gmail.com>
2017-11-07 20:26:54 -0700

    Updates for tech report PDF

--- R/tufte_utils.R
+++ R/tufte_utils.R
@@ -64,3 +64,58 @@ searchYAML <- function(input, element="includes"){
   yml <- yaml::yaml.load(paste(yml[-c(grep("---", yml), grep("[.][.][.]", yml))], collapse="\n"))
   if (!is.null(yml[[element]]))  return(yml[[element]])
 }
+
+
+scrubPDF <- function(input) {
+  md.text <- rmarkdown:::read_lines_utf8(file(input), getOption("encoding"))
+
+  #  Equations
+  # tmp.latex.eqn <- list()
+  # for(j in grep("[{]55pt[}]", md.text)) {
+  #   if (grepl(" [\\][\\] ", md.text[j])) tmp.latex.eqn[[j]] <- "multline" else tmp.latex.eqn[[j]] <- "equation"
+  #   md.text[j] <- gsub(".*[{]55pt[}]", paste("\\\\begin{", tmp.latex.eqn[[j]], "}", sep=""), md.text[j])
+  # }
+  # for(j in grep("[$][$]", md.text)) md.text[j] <- gsub("[$][$]", paste("\\\\end{", tmp.latex.eqn[[j]], "}", sep=""), md.text[j])
+
+  #  HTML Tag conversion
+  for(j in grep("<sup>th</sup>", md.text)) md.text[j] <- gsub("<sup>th</sup>", "$^{th}$", md.text[j])
+  for(j in grep("<sup>st</sup>", md.text)) md.text[j] <- gsub("<sup>st</sup>", "$^{st}$", md.text[j])
+  for(j in grep("<sup>rd</sup>", md.text)) md.text[j] <- gsub("<sup>rd</sup>", "$^{rd}$", md.text[j])
+  for(j in grep("<br></br>", md.text)) md.text[j] <- gsub("<br></br>", "\\\\", md.text[j])
+  for(j in grep("<em>", md.text)) md.text[j] <- gsub("<em>", "\\\\emph{", md.text[j])
+  for(j in grep("</em>", md.text)) md.text[j] <- gsub("</em>", "}", md.text[j])
+  for(j in grep("<strong>", md.text)) md.text[j] <- gsub("<strong>", "\\\\bf{", md.text[j])
+  for(j in grep("</strong>", md.text)) md.text[j] <- gsub("</strong>", "}", md.text[j])
+
+  for(j in grep("\\label[{]my[}]", md.text)) {
+    md.text[j] <- gsub("[{]my[}]", paste("{table", strsplit(strsplit(md.text[j], "[*][*]Table ")[[1]][2], ":[*][*]")[[1]][1], "}", sep=""), md.text[j])
+  }
+  for(j in grep("\\caption[{][*][*]Table", md.text)) {
+    md.text[j] <- gsub(":[*][*]", ":}", gsub("\\caption[{][*][*]Table", "\\caption*{\\\\textbf{Table", md.text[j]))
+  }
+
+  ## ztable doesn't have \caption
+  for(j in grep("[*][*]Table", md.text)) {
+    md.text[j] <- gsub("\\\\end", "}\\\\end", gsub(":[*][*]", ":}", gsub("[*][*]Table", "\\\\caption*{\\\\textbf{Table", md.text[j])))
+  }
+
+  if (any(grepl("<!-- HTML_Start", md.text))) {
+    if (length(grep("<!-- HTML_Start", md.text)) != length(grep("<!-- LaTeX_Start", md.text))){
+      stop("There must be equal number of '<!-- HTML_Start' and '<!-- LaTeX_Start' elements in the .Rmd file.")
+    }
+  }
+  while(any(grepl("<!-- HTML_Start", md.text))) {
+    html.index <- grep("<!-- HTML_Start", md.text)[1]
+    latex.index<- grep("<!-- LaTeX_Start", md.text)[1]
+    if (grepl("%", md.text[latex.index+1])) latex.index <- latex.index+1
+    md.text <- md.text[-(html.index:latex.index)]
+  }
+
+  md.text <- gsub("<!-- LaTeX_Start", "", md.text)
+  md.text <- gsub("LaTeX_End -->", "", md.text)
+
+  ## Get rid of random latex(...) comments
+  for(j in grep("%latex", md.text)) md.text[j] <- ""
+
+  writeLines(md.text, input)
+}
